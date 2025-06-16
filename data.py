28bdd41621926618c7d3e78013f5e5c332ef5ccc
Sonia Jain <soniajain@Sonias-MacBook-Pro.local>
2025-06-16 17:17:25 -0400

    test

--- data.py
+++ data.py
@@ -10,3 +10,6 @@ def getEodData():
     url = f'https://eodhd.com/api/eod/AAPL.US?from=2020-06-15&to=2025-06-15&period=d&api_token=demo&fmt=json'
     data = requests.get(url).json()
     return data
+
+
+print('test')
